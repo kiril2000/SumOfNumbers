558dd2ebd53e7f5b21054d4c5ee711c6df74bf32
Kiril Chobanov <62597135+kiril2000@users.noreply.github.com>
2020-04-05 15:26:48 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace SumOfNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 0;
+            int basket = 0;
+
+            number = int.Parse(Console.ReadLine());
+
+            while (number != 0)
+            {
+                basket += number;
+
+                number = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine(basket);
+        }
+    }
+}
